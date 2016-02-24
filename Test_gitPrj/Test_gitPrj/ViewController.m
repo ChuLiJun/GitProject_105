8e144ec7c1ddbc749fe55cf0ca4e26060889214a
chulujun <1994703260@qq.com>
2016-02-24 14:38:19 +0800

    chu
    chulijun

--- Test_gitPrj/Test_gitPrj/ViewController.m
+++ Test_gitPrj/Test_gitPrj/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     self.title = @"github 代码修改";
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //1234567890
+    
 }
 
 - (void)didReceiveMemoryWarning {
